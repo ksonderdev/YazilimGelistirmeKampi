541dea910a5866434b2158176b97b79ae52f31a1
Serkan Önder <sssonder72@gmail.com>
2021-03-19 23:05:24 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle( Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.soyAd + " " +"eklendi!");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.soyAd + " " +"silindi!");
+
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Adı : " + musteri.Ad + " " +"Müşteri Soyadı : " + musteri.soyAd + " " + "Müşteri Id : " + musteri.Id + " " +  " Müşteri Hesap No : " + musteri.HesapNo);
+
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Musteri musteri1 = new Musteri();
+                musteri1.Ad = "Serkan";
+                musteri1.soyAd = "Önder";
+                musteri1.Id = 1;
+                musteri1.HesapNo = 111;
+
+                Musteri musteri2 = new Musteri();
+                musteri2.Ad = "Dimitris";
+                musteri2.soyAd = "Pelkas";
+                musteri2.Id = 2;
+                musteri2.HesapNo = 112;
+
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] {musteri1 , musteri2};
+            musteriManager.MusteriListele(musteriler);
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriSil(musteri1);
+
+        }
+    }
+}
